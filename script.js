147a83c4b6fc79dbc98d92b396e68fc9cfab95ec
Mark Thorne <thorne.mark@gmail.com>
2022-01-05 14:40:38 -0600

    Initial test quote; additional styling

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let button = document.querySelector("#quoteButton");
 let quote = document.querySelector("#quote");
+let testQuote = `Arms and the man I sing, who first made way,
+  predestined exile, from the Trojan shore
+  to Italy, the blest Lavinian strand.
+  - Vergil, Aeneid 1.1-3`;
 
 button.addEventListener("click", () => {
-  quote.innerText = "Avoid evil, do what is good.";
-  quote.style.fontSize = "1.5rem";
+  quote.innerText = testQuote;
+  quote.style.fontSize = "1.2rem";
 });
