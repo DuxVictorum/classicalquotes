2276bdc6dfb5d9b8c30379a1e1557fec4a9a4246
Mark Thorne <thorne.mark@gmail.com>
2022-01-09 23:50:11 -0600

    Added ClassicalQuotes.js module

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+import quotes from "./classicalQuotes.js";
+
+let question = document.querySelector("#question");
 let button = document.querySelector("#quoteButton");
 let quote = document.querySelector("#quote");
-let testQuote = `Arms and the man I sing, who first made way,
-  predestined exile, from the Trojan shore
-  to Italy, the blest Lavinian strand.
-  - Vergil, Aeneid 1.1-3`;
 
 button.addEventListener("click", () => {
-  quote.innerText = testQuote;
-  quote.style.fontSize = "1.2rem";
+  let randQuote = Math.floor(Math.random() * quotes.length);
+  if (question.value === "") {
+    quote.innerText = "You have to ask a question first, that's how it works.";
+  } else {
+    quote.innerText = quotes[randQuote];
+    quote.style.fontSize = "1.2rem";
+  }
 });
