36e84bf6f4fd21b51024e1d0255a25dbe4155f93
Mark Thorne <thorne.mark@gmail.com>
2022-01-20 23:54:49 -0600

    Added more explanatory comments to the code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import quotes from "./classicalQuotes.js";
 
+// Initializing DOM variables
 let question = document.querySelector("#question");
 let button = document.querySelector("#quoteButton");
 let quoteIntro = document.querySelector("#quoteIntro");
@@ -8,20 +9,28 @@ let quote = document.querySelector("#quote");
 let author = document.querySelector("#author");
 let work = document.querySelector("#work");
 let citation = document.querySelector("#citation");
-quoteContent.style.display = "none";
 
 let questionsList = [];
 
+// Quote content begins hidden until button is pressed
+quoteContent.style.display = "none";
+
+// Event listener
 button.addEventListener("click", () => {
+  // Chooses one of the quotes at random
   let randQuote = Math.floor(Math.random() * quotes.length);
+
+  // If user enters nothing, error message appears.
   if (question.value === "") {
     quoteContent.style.display = "none";
     quoteIntro.innerHTML =
       "<p>Please ask a question first, that's how this works.</p>";
+    // If user enters the same question twice, a reminder appears to ask a new question.
   } else if (questionsList.includes(question.value)) {
     quoteContent.style.display = "none";
     quoteIntro.innerHTML =
       "<p>You've already asked that question.<br><br>Did you not like your answer?</p>";
+    // If user asks a new question, it is added to questionList and a new quote is displayed.
   } else {
     questionsList.push(question.value);
     quoteIntro.innerHTML = "<u>The ancient text says:</u>";
